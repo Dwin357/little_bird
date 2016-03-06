c54cedced667d7265bd7d6b49e63251642aa33d9
dwin357 <e.r.steinmetz@gmail.com>
2016-03-06 12:53:29 -0600

    switching gears from association problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,14 @@ class User < ActiveRecord::Base
   has_many :likes, foreign_key: :liker_id
   has_and_belongs_to_many :posts 
 
+
+##########  not applying scope, not clear why not ###
   has_many :followed, through: :watching
   has_many :followers, through: :being_watched
 
   # private
   has_many :watching, {class_name: :Following, foreign_key: :follower_id}, -> { where blocked: false }
   has_many :being_watched, {class_name: :Following, foreign_key: :followed_id}, -> { where blocked: false }
+#####################
 
 end
